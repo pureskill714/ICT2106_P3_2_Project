d5d5a4ab7f31b1b0442a81e86b1b3c29a3fd7f43
kyethefluffyowl <friendsbacktoback@gmail.com>
2023-03-27 20:21:50 +0800

    front-end
    yas queen

--- YouthActionDotNet/ClientApp/src/Pages/ServiceCenter/ServiceCenters.js
+++ YouthActionDotNet/ClientApp/src/Pages/ServiceCenter/ServiceCenters.js
@@ -1,143 +1,147 @@
-
-import React from "react"
-import { Button } from "reactstrap"
-import { Loading } from "../../Components/appCommon"
-import DatapageLayout from "../PageLayout"
+import React from "react";
+import { Button } from "reactstrap";
+import { Loading } from "../../Components/appCommon";
+import { ReactNotifications, Store } from "react-notifications-component";
+import DatapageLayout from "../PageLayout";
+import MyComponent from "../NotificationComponent";
 
 export default class ServiceCenters extends React.Component {
-    state={
-        content:null,
-        headers:[],
-        loading:true,
-        settings: {},
-        error: "",
-    }
+  state = {
+    content: null,
+    headers: [],
+    loading: true,
+    settings: {},
+    error: "",
+  };
 
-    settings ={
-        title:"Service Center",
-        primaryColor: "#a6192e",
-        accentColor: "#94795d",
-        textColor: "#ffffff",
-        textColorInvert: "#606060",
-        api: "/api/ServiceCenter/",
-    }
+  settings = {
+    title: "Service Center",
+    primaryColor: "#a6192e",
+    accentColor: "#94795d",
+    textColor: "#ffffff",
+    textColorInvert: "#606060",
+    api: "/api/ServiceCenter/",
+  };
 
-    async componentDidMount(){
-        await this.getContent().then((content)=>{
-            console.log(content);
-            this.setState({
-                content:content,
-            });
-        })
+  async componentDidMount() {
+    await this.getContent().then((content) => {
+      console.log(content);
+      this.setState({
+        content: content,
+      });
+    });
 
-        await this.getSettings().then((settings)=>{
-            console.log(settings);
-            this.setState({
-                settings:settings,
-            });
-        })
+    await this.getSettings().then((settings) => {
+      console.log(settings);
+      this.setState({
+        settings: settings,
+      });
+    });
 
+    this.setState({
+      loading: false,
+    });
+  }
+
+  test = (abc, def) => {};
+
+  getSettings = async () => {
+    // fetches http://...:5001/api/User/Settings
+    return fetch(this.settings.api + "Settings", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log(res);
+      return res.json();
+    });
+  };
+
+  getContent = async () => {
+    return fetch(this.settings.api + "All", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log(res);
+      return res.json();
+    });
+  };
+
+  update = async (data) => {
+    console.log(data);
+    return fetch(this.settings.api + "UpdateAndFetch/" + data.ServiceCenterId, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(async (res) => {
+      return res.json();
+    });
+  };
+
+  handleUpdate = async (data) => {
+    await this.update(data).then((content) => {
+      if (content.success) {
         this.setState({
-            loading:false,
-        })
-    }
-
-    test = (abc, def) => { 
-
-    }
-
-    getSettings = async () => {
-        // fetches http://...:5001/api/User/Settings
-        return fetch(this.settings.api + "Settings" , {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }).then(res => {
-            console.log(res);
-            return res.json();
-        })
-    }
-
-    getContent = async () =>{
-        return fetch( this.settings.api + "All" , {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }).then(res => {
-            console.log(res);
-            return res.json();
+          error: "",
         });
-    }
-
-    update = async (data) =>{
-        console.log(data);
-        return fetch(this.settings.api + "UpdateAndFetch/" + data.ServiceCenterId , {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data)
-        }).then(async res => {
-            return res.json();
+        return true;
+      } else {
+        this.setState({
+          error: content.message,
         });
-    }
+        return false;
+      }
+    });
+  };
 
-    handleUpdate = async (data) =>{
-        await this.update(data).then((content)=>{
-            if(content.success){
-                this.setState({
-                    error:"",
-                })
-                return true;
-            }else{
-                this.setState({
-                    error:content.message,
-                })
-                return false;
-            }
-        })
-    }
+  requestRefresh = async () => {
+    this.setState({
+      loading: true,
+    });
+    await this.getContent().then((content) => {
+      console.log(content);
+      this.setState({
+        content: content,
+        loading: false,
+      });
+    });
+  };
 
-    requestRefresh = async () =>{
-        this.setState({
-            loading:true,
-        })
-        await this.getContent().then((content)=>{
-            console.log(content);
-            this.setState({
-                content:content,
-                loading:false,
-            });
-        })
-    }
-    
-    requestError = async (error) =>{
-        this.setState({
-            error:error,
-        })
-    }
+  requestError = async (error) => {
+    this.setState({
+      error: error,
+    });
+  };
 
+  render() {
+    if (this.state.loading) {
+      return <Loading></Loading>;
+    } else {
+      return (
+        <div style={{ width: "100%" }}>
+          <ReactNotifications />
+          <div style={{ width: "2em" }}>
+            <MyComponent></MyComponent>
+          </div>
 
-    render(){
-        if(this.state.loading){
-            return <Loading></Loading>
-        }else{
-            return (
-            <DatapageLayout 
-                    settings={this.settings}
-                    fieldSettings={this.state.settings.data.FieldSettings}
-                    headers={this.state.settings.data.ColumnSettings}
-                    data={this.state.content.data}
-                    updateHandle={this.handleUpdate}
-                    requestRefresh={this.requestRefresh}
-                    error={this.state.error}
-                    permissions={this.props.permissions}
-                    requestError={this.requestError}
-                    >
-                </DatapageLayout>
-            )
-        }
+          <DatapageLayout
+            settings={this.settings}
+            fieldSettings={this.state.settings.data.FieldSettings}
+            headers={this.state.settings.data.ColumnSettings}
+            data={this.state.content.data}
+            updateHandle={this.handleUpdate}
+            requestRefresh={this.requestRefresh}
+            error={this.state.error}
+            permissions={this.props.permissions}
+            requestError={this.requestError}
+          ></DatapageLayout>
+        </div>
+      );
     }
+  }
 }
